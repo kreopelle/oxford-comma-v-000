1210ef852a0637dfe54a9778d3ffd2811ea3418e
Kayla Reopelle <kreopelle@gmail.com>
2018-04-08 17:40:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,10 @@
 def oxford_comma(array)
-  final = array.last
-  array.pop
-  string_version = "#{array.join(", ")}, and #{final}"
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
+  end
 end
